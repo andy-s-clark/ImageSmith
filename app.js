678c496a159e51fc7467326ced00cdbd850f7518
Dan Green <dan.green@build.com>
2013-11-13 16:44:15 -0800

    Creats a new route for uploading images. Removes some unecessary files.

--- app.js
+++ app.js
@@ -3,13 +3,16 @@
  * Module dependencies.
  */
 
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
+var express = require('express'),
+	routes = require('./routes'),
+	user = require('./routes/user'),
+	images = require('./routes/images'),
+	http = require('http'),
+	path = require('path'),
+	app = express()
+;
 
-var app = express();
+
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -30,6 +33,7 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/upload/image', images.upload);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
