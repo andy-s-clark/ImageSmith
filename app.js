f474802824663ed9725c9511745afd28d3f0156f
Geoff Lawson <geoff.lawson@build.com>
2013-11-14 06:55:13 -0800

    Added underscore, created 'list' for image files, added graphics magic wrapper.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
+var image = require('./routes/image');
 var http = require('http');
 var path = require('path');
 
@@ -15,12 +15,15 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('media', path.join(__dirname, 'media'));
+
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
+app.use(express.static(path.join(__dirname, 'media')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
@@ -29,7 +32,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/images/:bucket/:id', image.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
---
+++ routes/image.js
@@ -0,0 +1,57 @@
+
+/*
+ * GET images listing.
+ */
+
+exports.list = function (req, res) {
+	var fs = require('fs');
+	var mediaDir = req.app.get('media');
+
+	var bucket = req.param('bucket');
+	var id = req.param('id');
+
+	mediaDir = mediaDir + '/' + bucket + '/' + id;
+
+	fs.readdir(mediaDir, function (err, list) {
+		var files = [];
+		var rx = new RegExp(/\.(gif|jpg|jpeg|tiff|png)$/);
+
+		if (list) {
+			for(var i = 0; i < list.length; i++) {
+				var file = list[i];
+				var stat = fs.statSync(mediaDir + '/' + file);
+				var path = '/media/' + bucket + '/' + id + '/' + file;
+
+				if (stat.isFile() && rx.test(file)) files.push(path);
+			};
+		};
+
+		res.render('image_list', { title: 'Media List:', bucket: bucket, id: id, files: files, error: err });
+	});
+};
+
+/*
+ * POST new image
+ */
+
+exports.post = function(req, res) {
+	var fs = require('fs');
+	var mediaDir = req.app.get('media');
+
+	var bucket = req.param('bucket');
+	var id = req.param('id');
+
+	mediaDir = mediaDir + '/' + bucket + '/' + id;
+
+	var gm = require('gm');
+	var im = gm.subClass({ imageMagick: true });
+
+	im('/Users/g_lawson/Pictures/bender.jpg')
+	.resize(100, 100)
+	.autoOrient()
+	.write(mediaDir + '/test.jpg', function (err) {
+		if (!err) console.log(' hooray! ');
+	});
+
+
+};
